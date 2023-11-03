e87b1b5cfd1aed65a5d8f40dfa54bce786c36ed9
imloadinqqq <107895308+imloadinqqq@users.noreply.github.com>
2023-11-03 12:47:31 -0500

    Added starter file

---
+++ service.py
@@ -0,0 +1,10 @@
+#
+# Computer Networking (CS 3840)
+# Team Members: Alex Diaz, Ryan Luter, Logan Quinley, and Ryan Smith
+#
+# Description: 
+#    Our goal for this project is to create a web server that will generate a
+#    page that will play rock paper scissors
+#
+
+import socket
